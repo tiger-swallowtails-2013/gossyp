c12c942c9a4786f664ee6ad2ba6cc275fcc77a2f
Zee Spencer <zee@zeespencer.com>
2013-09-30 09:53:23 -0700

    Outline Guest May Login With Twitter feature

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+
+
+# By grouping our testing gems in development and test we prevent them from
+# being installed on Heroku.
+#
+# http://bundler.io/v1.3/groups.html
+group :development, :test do
+  gem 'rspec'
+end
---
+++ spec/features/guest_logs_in_with_twitter_feature.rb
@@ -0,0 +1,11 @@
+describe "Guest logs in with twitter" do
+  it "redirects them to the home page"
+  it "displays a welcome! message"
+
+  context "when they have not logged in before" do
+    it "creates a new user"
+  end
+  context "when they have logged in before" do
+    it "does not create a new user"
+  end
+end
