6958134bd205fff3a04b7a5bfbd28dc8fac44a36
Zee Spencer <zee@zeespencer.com>
2013-09-30 14:40:39 -0700

    Configure app to run on heroku
    * Prevent dotenv from being loaded in production and staging
    * Add config.ru

--- initializers/dotenv.rb
+++ initializers/dotenv.rb
@@ -1,6 +1,18 @@
-# dotenv allows us to store configuration variables in a .env file
-# This makes it easy to keep our secret configuration variables
-# outside of our public repo. HOORAY!
-require 'dotenv'
-Dotenv.load('.env.development')
-# https://github.com/bkeepers/dotenv#sinatra-or-plain-ol-ruby
+# Heroku by default allows you to set environment variables with heroku
+# config:set VARIABLE=value
+
+# See:
+# https://devcenter.heroku.com/articles/config-vars#setting-up-config-vars-for-a-deployed-application
+
+# Because of this, we don't want to load dotenv in production or staging.
+# See http://www.sinatrarb.com/configuration.html#built-in-settings
+
+
+unless ["production", "staging"].include? ENV['RACK_ENV']
+  # dotenv allows us to store configuration variables in a .env file
+  # This makes it easy to keep our secret configuration variables
+  # outside of our public repo. HOORAY!
+  require 'dotenv'
+  Dotenv.load('.env.development')
+  # https://github.com/bkeepers/dotenv#sinatra-or-plain-ol-ruby
+end
