4b18d519f89d7d3f8ea90814d76dc67a1e300d5e
Zee Spencer <zee@zeespencer.com>
2013-09-30 12:16:53 -0700

    Extract shared spec_helper for feature and integration tests

--- spec/features/guest_logs_in_with_twitter_feature.rb
+++ spec/features/guest_logs_in_with_twitter_feature.rb
@@ -1,24 +1,5 @@
-# We set the load path so we can avoid require_relative
-$LOAD_PATH.unshift(File.expand_path('.'))
-# File::expand_path - http://www.ruby-doc.org/core-2.0.0/File.html#method-c-expand_path
-# $LOAD_PATH - http://www.ruby-doc.org/core-2.0/Kernel.html#method-i-require
-# Array#unshift http://www.ruby-doc.org/core-2.0.0/Array.html#method-i-unshift
-
-# We require our app so that we have access to Sinatra::Application for capybara
-require 'gossyp'
-
-require 'capybara/rspec'
-
-# Tell capybara that we're testing your sinatra app
-# https://github.com/jnicklas/capybara#setup
-Capybara.app = Sinatra::Application
-
-# Turn on test mode fo omniauth so it doesn't actually hit twitter
-OmniAuth.config.test_mode = true
-# https://github.com/intridea/omniauth/wiki/Integration-Testing
-
-
-# If we want to use rspec without rails, we need to specify the
+require 'spec_helper'
+# If we want to use capybara without rails, we need to specify the
 # type: :feature option for the rspec describe block
 describe "Guest logs in with twitter", type: :feature do
 # https://github.com/jnicklas/capybara#using-capybara-with-rspec
---
+++ spec/spec_helper.rb
@@ -0,0 +1,18 @@
+# We set the load path so we can avoid require_relative
+$LOAD_PATH.unshift(File.expand_path('.'))
+# File::expand_path - http://www.ruby-doc.org/core-2.0.0/File.html#method-c-expand_path
+# $LOAD_PATH - http://www.ruby-doc.org/core-2.0/Kernel.html#method-i-require
+# Array#unshift http://www.ruby-doc.org/core-2.0.0/Array.html#method-i-unshift
+
+# We require our app so that we have access to Sinatra::Application for capybara
+require 'gossyp'
+
+require 'capybara/rspec'
+
+# Tell capybara that we're testing your sinatra app
+# https://github.com/jnicklas/capybara#setup
+Capybara.app = Sinatra::Application
+
+# Turn on test mode fo omniauth so it doesn't actually hit twitter
+OmniAuth.config.test_mode = true
+# https://github.com/intridea/omniauth/wiki/Integration-Testing
