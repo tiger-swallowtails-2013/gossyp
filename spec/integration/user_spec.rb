4494ff502dbe47cb4d427489e2ef3da99f17f418
Zee Spencer <zee@zeespencer.com>
2013-09-30 12:26:12 -0700

    User.from_auth_hash returns a user

--- spec/integration/user_spec.rb
+++ spec/integration/user_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 describe User do
   describe ".from_auth_hash" do
+    it "returns a user with a twitter_uid which matches the passed in uid" do
+      user = User.from_auth_hash({
+        uid: 12345
+      })
+      expect(user.twitter_uid).to eql(12345)
+    end
+
     context "when the user does not yet exist" do
       it "creates a new user with the provider uid" do
         User.from_auth_hash({
@@ -9,6 +16,7 @@ describe User do
         expect(User.find_by(twitter_uid: 12345)).not_to be_nil
       end
     end
+
     context "when the user does exist" do
       it "does not create a new user" do
         User.create(twitter_uid: 45677)
