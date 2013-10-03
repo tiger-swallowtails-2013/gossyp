a7313231ecef49c07e0b9de9da955e35c5e67720
Zee Spencer <zee@zeespencer.com>
2013-10-03 11:19:43 -0700

    Outline starring feature

---
+++ spec/features/gossyper_may_star_gossyp_feature.rb
@@ -0,0 +1,8 @@
+describe 'Gossyper may star gossyp' do
+  context "when the gossyp is not yet starred" do
+    it "stars the gossyp!"
+  end
+  context "when the gossyp is starred" do
+    it "unstars the gossyp"
+  end
+end
